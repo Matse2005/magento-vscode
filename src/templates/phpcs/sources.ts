eb28a7359027163d63619dac9e4e20aeea1189db
Matse Van Horebeek <matse.vanhorebeek@phpro.be>
2026-03-04 10:39:45 +0100

    Yeah

---
+++ src/templates/phpcs/sources.ts
@@ -0,0 +1,5 @@
+import type { SourceItem, SourceContext } from '../../services/templateRegistry';
+
+// No sources defined for this template.
+// Add exports here if you add select/multi-select steps later.
+export default {};
